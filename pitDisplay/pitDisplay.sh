0af4ade61766e6cb74f85dfb4d7930d6f0431af2
Andrew Kempen <akemperfi@gmail.com>
2017-03-21 19:49:52 -0500

    Fixed issue with pitDisplay slide show path

--- pitDisplay/pitDisplay.sh
+++ pitDisplay/pitDisplay.sh
@@ -1,4 +1,4 @@
 #DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/bin/feh -q -p -Z -F -R  60 -Y -D 5.0 -B white /home/pi/CORE2017-Vision/pitDisplay/slides
 #DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority 
-/usr/bin/libreoffice --nologo --invisible --minimized --nofirststartwizard --nodefault --norestore --show slides.odp
+/usr/bin/libreoffice --nologo --invisible --minimized --nofirststartwizard --nodefault --norestore --show /home/pi/CORE2017-Vision/pitDisplay/slides.odp
 
